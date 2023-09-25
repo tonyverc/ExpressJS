f518e9fc23061f4f283516f2e074ba155dd1262c
tonyverc <tony.vercruysse@hotmail.com>
2023-09-25 16:38:49 +0200

    amélioration de la page d'accueil

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>hello world!</title>
+  </head>
+  <body>
+      <h1>hello world!</h1>
+  </body>
+  </html>
+  `
+  res.send(html)
 })
 
 app.listen(port, () => {
